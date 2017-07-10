8b5f3ef6c7b6ec376edbd59fd762c56adc449dcc
ryoukawa <ryou1110.pkmn@gmail.com>
2017-07-10 18:47:58 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,26 +1,22 @@
-
- 
- var number = Math.floor(Math.random()*101);
- var result=0;
+var number = Math.floor(Math.random()*101);
+var result=0;
 while(1){
- var answer =parseInt(window.prompt('数あてゲーム0~100の数字を入力して下さい'));
- 
- var message;
- if(answer === number){
-   message = '凄いあたり！';
-   break;
-   }else if (answer < number){
-    result  = '残念！もっと大きい数字だよ！このまま続ける？';
-    }else if (number < answer ){
-   result = '残念！もっと小さい数字だよ！このまま続ける？';
-    }else{
-    result  = '0~100の数字以外は入れないでねこのまま続ける？';
-   }
+var answer = parseInt(window.prompt('数あてゲーム。0～100の数字を入力してね！'));
+
+var message;
+if(answer === number){
+  message =　'凄い当たり！';
+  break;
+}
+  else if (answer < number){
+result = window.confirm('残念！もっと大きい数字だよ。このまま続ける？');
+}else if (number < answer){
+ result = window.confirm('残念！もっと小さい数字だよ。このまま続ける？');
+}else{
+  result = window.confirm('0～100の数字を入力してね。このまま続ける？');
+}
   if (result===true)
     result=0;
   else　break;
-  
-}
+ }
 document.getElementById('choice').textContent = message;
-
-   
